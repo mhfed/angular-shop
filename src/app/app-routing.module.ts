9522fee9c3ad94733fd85b2318ed21f0a621b7d8
hieufe <hieunm@metacity.email>
2022-05-26 20:12:04 +0700

    build enviroment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
+import { AdminComponent } from './layouts/admin/admin.component';
+import { WebsiteComponent } from './layouts/website/website.component';
+
 import { RouterModule, Routes } from '@angular/router';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ProductsComponent } from './pages/products/products.component';
+import { ProductComponent } from './pages/admin/product/product.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'product', component: ProductsComponent },
-  { path: '**', component: NotfoundComponent },
+  { path: '', component: WebsiteComponent },
+  {
+    path: 'admin',
+    component: AdminComponent, // this is the component with the <router-outlet> in the template
+    children: [
+      {
+        path: 'product', // child route path
+        component: ProductComponent, // child route component that the router renders
+      },
+    ],
+  },
+  // { path: 'home', component: HomeComponent },
+  // { path: 'about', component: AboutComponent },
+  // { path: 'product', component: ProductsComponent },
+  // { path: '**', component: NotfoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/layout/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-import { FooterComponent } from './components/layout/footer/footer.component';
 import { TableComponent } from './components/table/table.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
-import { ProductsComponent } from './pages/products/products.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AdminComponent } from './layouts/admin/admin.component';
+import { WebsiteComponent } from './layouts/website/website.component';
+import { SidebarComponent } from './shared/admin/sidebar/sidebar.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ProductAddComponent } from './pages/admin/product-add/product-add.component';
+import { ProductEditComponent } from './pages/admin/product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HomeComponent,
     AboutComponent,
-    FooterComponent,
     TableComponent,
     NotfoundComponent,
-    ProductsComponent,
+    AdminComponent,
+    WebsiteComponent,
+    SidebarComponent,
+    ProductComponent,
+    ProductAddComponent,
+    ProductEditComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
